78e23b13c97371829d8c91c655f1a7baf0618c45
Monte C. Roden <rodenmonte@gmail.com>
2016-07-23 17:16:38 -0700

    Guessing feature working

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,7 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-the_number = rand(100) + 1
+number = rand(100) + 1
 get '/' do
-  erb :index, :locals => { :number => the_number}
+  guess = params['guess']
+  message = check_guess(guess, number)
+  erb :index, :locals => { :number => number, :message => message}
+end
+
+def check_guess(guess, number)
+  if guess == "" || guess == nil
+    message = "Guess the magic number!"
+  elsif guess.to_i > number
+    if guess.to_i - 5 <= number
+      message = "Too high, try again. You are very close."
+    else
+      message = "Too high, try again."
+    end
+  elsif guess.to_i < number
+    if guess.to_i + 5 >= number
+      message = "Too low, try again. You are very close."
+    else
+      message = "Too low, try again."
+    end
+  else
+    message = "You're right! The secret number is #{number}"
+  end
 end
